52e94d0ca01a46e5e220a5732555183ea77eaba2
Tommy Han <tommy@Tommys-Air.home>
2020-05-15 11:27:28 -0400

    initial version.

---
+++ client/script.js
@@ -0,0 +1,37 @@
+// var button = document.getElementsByTagName("button")[0];
+
+// button.addEventListener("click", function(){
+//     console.log("CLICK !!!!");
+// })
+
+
+var button = document.getElementById("enter");
+var input = document.getElementById("userInput");
+var ul = document.querySelector("ul");
+
+function inputLength(){
+    return input.value.length;
+}
+
+function createListElement(){
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
+}
+
+
+function addListAfterClick(){
+    if (inputLength() > 0){
+        createListElement();
+    }
+}
+
+function addListAfterKeyPress(){
+    if (inputLength() > 0 && event.keyCode == 13){
+        createListElement();
+    }
+}
+
+button.addEventListener("click",addListAfterClick);
+input.addEventListener("keypress",addListAfterKeyPress);

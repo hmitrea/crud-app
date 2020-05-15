80163abdc67ef697ff62430324317701711e43de
lindaeverswick <lindaeverswick@gmail.com>
2020-05-15 13:40:54 -0400

    added models file

--- server/models/models.js
+++ server/models/models.js
@@ -1,1 +1,3 @@
 const mongoose = require('mongoose');
+
+const MONGO_URI = 'mongodb+srv://horatiu:waterbottle@cluster0-ryq0v.mongodb.net/test?retryWrites=true&w=majority'
--- server/server.js
+++ server/server.js
@@ -1,20 +1,29 @@
 const path = require("path");
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const port = 3000;
 
-app.get("/", (req, res) => res.render("./../client/index"));
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./../client/index.html"));
+});
 
-app.post("/signup", (req, res) => res.send("sign up successful"));
+app.post("/signup", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "./../client/index.html"))
+);
 
 app.put("/api/shoppingCart", (req, res) => res.send("successful put request"));
+
 app.post("/api/shoppingCart", (req, res) =>
   res.send("successful post request")
 );
+
 app.delete("/api/shoppingCart", (req, res) =>
   res.send("successful delete request")
 );
+
 app.get("/api/shoppingCart", (req, res) => res.send("successful get request"));
+
 app.listen(port, () => console.log(`server listening on port ${port}`));
 
 module.exports = app;

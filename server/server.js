ccc48ac52cade63e902734b3c82443aa3e9b9777
Tommy Han <tommy@Tommys-Air.home>
2020-05-15 12:52:54 -0400

    added client index html

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.render("./../client/index"));
+app.get("/", (req, res) => res.sendFile(path.resolve(__dirname, "../client/index.html")));
 
 app.post("/signup", (req, res) => res.send("sign up successful"));
 
